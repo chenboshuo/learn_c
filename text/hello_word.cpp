8753cedc51c7260be40dd04c3dcae91d8edf8696
yichuanshuzi <chenboshuo.hi@163.com>
2018-12-10 19:49:00 +0800

    the first program

--- text/hello_word.cpp
+++ text/hello_word.cpp
@@ -1,7 +1,12 @@
-#include<stdio.h>\\包含标准库的信息
+#include<stdio.h>//包含标准库的信息
 
-main() \\定义为main的函数,它不接受参数值
+main() //定义为main的函数,它不接受参数值
 
 {
 	printf("hello world\n");
+	printf("hello ");
+	printf("world\n");
  }
+// output
+// hello world
+// hello world
