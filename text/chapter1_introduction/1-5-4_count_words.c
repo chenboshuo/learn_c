c7c2f204ab1db04a27760f6ba21d0c0400306557
yichuanshuzi <chenboshuo.hi@163.com>
2018-12-18 15:07:13 +0800

    统计输入的行数,单词数与字符数

--- text/chapter1_introduction/1-5-4_count_words.c
+++ text/chapter1_introduction/1-5-4_count_words.c
@@ -3,7 +3,14 @@
 #define IN 1 //在单词之内
 #define OUT 0 //在单词之外
 
-/* 统计输入的行数,单词数与字符数 */
+/* 统计输入的行数,单词数与字符数
+input
+this is a test.
+this is a test.
+^Z
+output
+2 8 32
+  */
 int main() {
   int c, nl, nw, nc, state;
   state = OUT;
@@ -16,11 +23,12 @@ int main() {
       /*
        ||  代表逻辑或 && 代表与,
        由两者组成的表达式从左到右求值
+       && 仅仅比||高一个优先级
        */
       state = IN;
       ++nw;
     }
   }
   printf("%d %d %d\n", nl, nw, nc);
-  getchar();
+  // getchar();
 }
