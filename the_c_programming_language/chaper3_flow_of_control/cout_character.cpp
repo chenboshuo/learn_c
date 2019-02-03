c005fd32ad097d40e23626addbe7c6e31bfc4619
yichuanshuzi <chenboshuo.hi@163.com>
2019-02-03 09:22:52 +0800

    3.4 switch 语句

--- the_c_programming_language/chaper3_flow_of_control/cout_character.cpp
+++ the_c_programming_language/chaper3_flow_of_control/cout_character.cpp
@@ -10,13 +10,22 @@ int main() {
   for ( i = 0; i < 10; i++)
     ndigit[i] = 0;
 
-  while ((c = getchar()) != EOF)
-    if (c >= '0' && c <= '9')//判断 c 是否为数字
-      ++ndigit[c - '0'];
-    else if (c == ' ' || c == '\n' || c == '\t')
-      ++nwhite;
-    else
-      ++nother;
+  while ((c = getchar()) != EOF){
+    switch (c) {
+    case '0':case '1':case '2':case'3':case '4':
+    case '5':case '6':case'7':case'8':case'9':
+      ndigit[c-'0']++;
+      break;
+    case ' ':
+    case'\n':
+    case'\t':
+      nwhite++;
+      break;
+    default:
+      nother++;
+      break;
+    }
+  }
   printf("digits = ");
   for ( i = 0; i < 10; i++)
     printf("%d\n", ndigit[i]);
@@ -24,3 +33,20 @@ int main() {
       nwhite, nother);
   getchar();
 }
+// input
+// 4 0 4
+// zsb
+// ^Z
+// output
+// digits = 1
+// 0
+// 0
+// 0
+// 2
+// 0
+// 0
+// 0
+// 0
+// 0
+// , white space = 5, other = 3
+//
