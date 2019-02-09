3387bbfb8a830184124af289e2030438d2b2c659
yichuanshuzi <chenboshuo.hi@163.com>
2019-02-09 15:52:33 +0800

    第四章 函数与程序结构

--- the_c_programming_language/赋值号.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-main()
-{int x = 3;
-	x+= x -= x*x;//¸³ÖµºÅ´ÓÓÒÍù×ó¼ÆËã 
-	printf("%d",x) ;
- } 
--- the_c_programming_language/输入输出.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-main()
-{
-	int c
-	c = getchar()
-	putchar(c)
- } 
