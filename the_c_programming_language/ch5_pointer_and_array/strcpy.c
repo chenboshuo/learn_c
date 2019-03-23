7edfbfaff27b1c1e7ec0db52749cd64507e72694
yichuanshuzi <chenboshuo.hi@163.com>
2019-03-23 09:47:18 +0800

    使用指针实现的版本1

--- the_c_programming_language/ch5_pointer_and_array/strcpy.c
+++ the_c_programming_language/ch5_pointer_and_array/strcpy.c
@@ -1,15 +1,13 @@
 /**
  * strcpy函数: 将指针t指向的字符串复制到s指向的位置
- * 使用数组下标实现的版本
+ * 使用指针实现的版本1
  */
 #include <stdio.h>
 
 void strcpy(char *s, char *t) {
-  int i;
-
-  i = 0;
-  while ((s[i] = t[i]) != '\0') {
-    i++;
+  while ((*s = *t) != '\0') {
+    s++;
+    t++;
   }
 }
 
