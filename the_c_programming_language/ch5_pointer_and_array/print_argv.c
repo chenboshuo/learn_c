0fe34dd9ecacf5eb05fae043102a85db313fb4b3
yichuanshuzi <chenboshuo.hi@163.com>
2019-04-13 18:11:02 +0800

    echo 的第二个版本在对argv进行自增运算,argc做自减运算实现的, argv是指向指针的指针.

--- the_c_programming_language/ch5_pointer_and_array/print_argv.c
+++ the_c_programming_language/ch5_pointer_and_array/print_argv.c
@@ -2,20 +2,13 @@
 
 /* 回显命令行参数: 版本1 */
 int main(int argc, char const *argv[]) {
-  int i;
-
-  for( i = 0; i < argc; ++i){
-    printf("%s%s\n", argv[i], (i < argc-1) ? " ": "");
-  }
+  while(--argc > 0)
+    printf("%s%s", *++argv, (argc > 1) ? " " : "");
   printf("\n");
   return 0;
 }
 /*
-$ print_argv 1 2 hello 5
-print_argv
-1
-2
-hello
-5
+$ print_argv a b 12 as
+a b 12 as
 
  */
